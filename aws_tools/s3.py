9993873d2de5473989d898b36c608520a0095cfa
NICOLAS REYNOSO <nicolas.reynoso@headwaydigital.com>
2018-08-06 12:34:55 -0300

    build get_s3_client so it can be pathched in development (e.g. to use localstack)

--- aws_tools/s3.py
+++ aws_tools/s3.py
@@ -1,9 +1,27 @@
 import boto3
+from os import environ
+
 botosess = boto3.session.Session()
+
+def get_s3_client(endpoint_url=None):
+    """ Returns an s3 session"""
+    kwargs = {}
+    if endpoint_url:
+        kwargs['endpoint_url'] = endpoint_url
+    s3 = botosess.client('s3', 'us-east-1', **kwargs)
+    return s3
+
+def get_s3_resource(endpoint_url=None):
+    kwargs = {}
+    if endpoint_url:
+        kwargs['endpoint_url'] = endpoint_url
+    s3 = botosess.resource('s3', 'us-east-1', **kwargs)
+    return s3
 
 def upload_file(bucket, key, data, public=False):
     '''Upload object file to S3'''
-    s3 = botosess.client('s3', 'us-east-1')
+    s3 = get_s3_client()
+
     try:
         if public:
             s3.upload_fileobj(
@@ -18,9 +36,10 @@ def upload_file(bucket, key, data, public=False):
 
 def download_file(bucket, key, date):
     '''download data to s3'''
-    s3 = botosess.client('s3', 'us-east-1')
+    s3 = get_s3_client()
     s3.download_file(bucket, key, '/tmp/id-{}.txt'.format(date))
 
 def get_object(bucket, key):
-    s3 = botosess.resource('s3')
+    s3 = get_s3_resource()
     return s3.Object(bucket_name=bucket, key=key)
+
