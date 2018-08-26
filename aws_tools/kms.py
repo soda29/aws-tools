452821c2e101486d61582216553f354a62f6a258
NICOLAS REYNOSO <nicolas.reynoso@headwaydigital.com>
2018-08-26 13:50:58 -0300

    Get KMS to use boto sessions so it could be used with asyncio without breaking

--- aws_tools/kms.py
+++ aws_tools/kms.py
@@ -1,8 +1,31 @@
 import boto3
 import base64
 
+botosess = boto3.session.Session()
+
+
+def get_kms_client(endpoint_url=None):
+    """ Returns an kms session"""
+    kwargs = {}
+    if endpoint_url:
+        kwargs['endpoint_url'] = endpoint_url
+    kms = botosess.client('kms', 'us-east-1', **kwargs)
+    return kms
+
+
+def get_kms_resource(endpoint_url=None):
+    kwargs = {}
+    if endpoint_url:
+        kwargs['endpoint_url'] = endpoint_url
+    kms = botosess.resource('kms', 'us-east-1', **kwargs)
+    return s3
+
+
 def decrypt(encrypted):
     '''Decrypt'''
-    decrypted = boto3.client('kms', region_name='us-east-1').decrypt(CiphertextBlob=base64.b64decode(
-        encrypted))['Plaintext']
+
+    kms = get_kms_client()
+    bs64str = base64.b64decode(encrypted)
+    decrypted = kms.decrypt(CiphertextBlob=b64str)['Plaintext']
     return decrypted
+
